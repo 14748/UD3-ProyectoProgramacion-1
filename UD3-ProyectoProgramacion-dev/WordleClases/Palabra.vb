51e5bed6e8f408899a7bbbe8ffe6051b5f0b9187
XabierAPC <124996462+XabierAPC@users.noreply.github.com>
2023-04-17 10:23:26 +0200

    Cambios despues Semana Santa
    Cambios Realizados en Diccionario y pruebas de borrar letras en Form1

---
+++ UD3-ProyectoProgramacion-dev/WordleClases/Palabra.vb
@@ -0,0 +1,23 @@
+﻿Public Class Palabra
+    Public Property Texto As String
+    Public Property Dificultad As Integer
+    Public Property NumeroLetras As Integer
+
+    Public Sub New(textoPasado As String, dificultadPasada As Integer, numeroLetras As Integer)
+        Me.Texto = textoPasado
+        Me.Dificultad = dificultadPasada
+        Me.NumeroLetras = numeroLetras
+    End Sub
+
+    Public Overrides Function Equals(obj As Object) As Boolean
+        Dim palabra = TryCast(obj, Palabra)
+        Return palabra IsNot Nothing AndAlso
+               Texto = palabra.Texto
+    End Function
+
+    Public Overrides Function GetHashCode() As Integer
+        Dim hashCode As Long = -356309981
+        hashCode = (hashCode * -1521134295 + EqualityComparer(Of String).Default.GetHashCode(Texto)).GetHashCode()
+        Return hashCode
+    End Function
+End Class
