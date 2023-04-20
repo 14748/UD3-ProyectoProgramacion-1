45879e97b26f2649f2ebb4c287f02a6f0ab01b5c
innombrable234 <116067431+innombrable234@users.noreply.github.com>
2023-04-20 09:42:12 +0200

    Add files via upload

--- Wordle/Form1.Designer.vb
+++ Wordle/Form1.Designer.vb
@@ -20,19 +20,455 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnbarras = New System.Windows.Forms.Button()
+        Me.btncal = New System.Windows.Forms.Button()
+        Me.lbllapalabra = New System.Windows.Forms.Label()
+        Me.lbldeldia = New System.Windows.Forms.Label()
+        Me.btnopc = New System.Windows.Forms.Button()
+        Me.btnconfig = New System.Windows.Forms.Button()
+        Me.btnELIMINAR = New System.Windows.Forms.Button()
+        Me.btnENVIAR = New System.Windows.Forms.Button()
+        Me.btnM = New System.Windows.Forms.Button()
+        Me.btnN = New System.Windows.Forms.Button()
+        Me.btnB = New System.Windows.Forms.Button()
+        Me.btnV = New System.Windows.Forms.Button()
+        Me.btnC = New System.Windows.Forms.Button()
+        Me.btnX = New System.Windows.Forms.Button()
+        Me.btnZ = New System.Windows.Forms.Button()
+        Me.btnÑ = New System.Windows.Forms.Button()
+        Me.btnL = New System.Windows.Forms.Button()
+        Me.btnK = New System.Windows.Forms.Button()
+        Me.btnJ = New System.Windows.Forms.Button()
+        Me.btnH = New System.Windows.Forms.Button()
+        Me.btnG = New System.Windows.Forms.Button()
+        Me.btnF = New System.Windows.Forms.Button()
+        Me.btnD = New System.Windows.Forms.Button()
+        Me.btnS = New System.Windows.Forms.Button()
+        Me.btnA = New System.Windows.Forms.Button()
+        Me.btnP = New System.Windows.Forms.Button()
+        Me.btnO = New System.Windows.Forms.Button()
+        Me.btnI = New System.Windows.Forms.Button()
+        Me.btnU = New System.Windows.Forms.Button()
+        Me.btnY = New System.Windows.Forms.Button()
+        Me.btnT = New System.Windows.Forms.Button()
+        Me.btnR = New System.Windows.Forms.Button()
+        Me.btnE = New System.Windows.Forms.Button()
+        Me.btnW = New System.Windows.Forms.Button()
+        Me.btnQ = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.SuspendLayout()
+        '
+        'btnbarras
+        '
+        Me.btnbarras.Image = CType(resources.GetObject("btnbarras.Image"), System.Drawing.Image)
+        Me.btnbarras.Location = New System.Drawing.Point(124, 38)
+        Me.btnbarras.Name = "btnbarras"
+        Me.btnbarras.Size = New System.Drawing.Size(42, 39)
+        Me.btnbarras.TabIndex = 89
+        Me.btnbarras.UseVisualStyleBackColor = True
+        '
+        'btncal
+        '
+        Me.btncal.Image = CType(resources.GetObject("btncal.Image"), System.Drawing.Image)
+        Me.btncal.Location = New System.Drawing.Point(219, 38)
+        Me.btncal.Name = "btncal"
+        Me.btncal.Size = New System.Drawing.Size(42, 39)
+        Me.btncal.TabIndex = 91
+        Me.btncal.UseVisualStyleBackColor = True
+        '
+        'lbllapalabra
+        '
+        Me.lbllapalabra.Font = New System.Drawing.Font("Segoe UI", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbllapalabra.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lbllapalabra.Location = New System.Drawing.Point(325, 19)
+        Me.lbllapalabra.Name = "lbllapalabra"
+        Me.lbllapalabra.Size = New System.Drawing.Size(167, 39)
+        Me.lbllapalabra.TabIndex = 92
+        Me.lbllapalabra.Text = "LA PALABRA"
+        '
+        'lbldeldia
+        '
+        Me.lbldeldia.Font = New System.Drawing.Font("Segoe UI", 15.25!)
+        Me.lbldeldia.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lbldeldia.Location = New System.Drawing.Point(357, 58)
+        Me.lbldeldia.Name = "lbldeldia"
+        Me.lbldeldia.Size = New System.Drawing.Size(91, 39)
+        Me.lbldeldia.TabIndex = 93
+        Me.lbldeldia.Text = "DEL DIA"
+        '
+        'btnopc
+        '
+        Me.btnopc.Image = CType(resources.GetObject("btnopc.Image"), System.Drawing.Image)
+        Me.btnopc.Location = New System.Drawing.Point(548, 42)
+        Me.btnopc.Name = "btnopc"
+        Me.btnopc.Size = New System.Drawing.Size(41, 37)
+        Me.btnopc.TabIndex = 94
+        Me.btnopc.UseVisualStyleBackColor = True
+        '
+        'btnconfig
+        '
+        Me.btnconfig.Image = CType(resources.GetObject("btnconfig.Image"), System.Drawing.Image)
+        Me.btnconfig.Location = New System.Drawing.Point(636, 42)
+        Me.btnconfig.Name = "btnconfig"
+        Me.btnconfig.Size = New System.Drawing.Size(43, 37)
+        Me.btnconfig.TabIndex = 95
+        Me.btnconfig.UseVisualStyleBackColor = True
+        '
+        'btnELIMINAR
+        '
+        Me.btnELIMINAR.Image = CType(resources.GetObject("btnELIMINAR.Image"), System.Drawing.Image)
+        Me.btnELIMINAR.Location = New System.Drawing.Point(570, 525)
+        Me.btnELIMINAR.Name = "btnELIMINAR"
+        Me.btnELIMINAR.Size = New System.Drawing.Size(66, 50)
+        Me.btnELIMINAR.TabIndex = 124
+        Me.btnELIMINAR.UseVisualStyleBackColor = True
+        '
+        'btnENVIAR
+        '
+        Me.btnENVIAR.Location = New System.Drawing.Point(172, 525)
+        Me.btnENVIAR.Name = "btnENVIAR"
+        Me.btnENVIAR.Size = New System.Drawing.Size(61, 50)
+        Me.btnENVIAR.TabIndex = 123
+        Me.btnENVIAR.Text = "ENVIAR"
+        Me.btnENVIAR.UseVisualStyleBackColor = True
+        '
+        'btnM
+        '
+        Me.btnM.Location = New System.Drawing.Point(523, 525)
+        Me.btnM.Name = "btnM"
+        Me.btnM.Size = New System.Drawing.Size(41, 50)
+        Me.btnM.TabIndex = 122
+        Me.btnM.Text = "M"
+        Me.btnM.UseVisualStyleBackColor = True
+        '
+        'btnN
+        '
+        Me.btnN.Location = New System.Drawing.Point(476, 525)
+        Me.btnN.Name = "btnN"
+        Me.btnN.Size = New System.Drawing.Size(41, 50)
+        Me.btnN.TabIndex = 121
+        Me.btnN.Text = "N"
+        Me.btnN.UseVisualStyleBackColor = True
+        '
+        'btnB
+        '
+        Me.btnB.Location = New System.Drawing.Point(429, 525)
+        Me.btnB.Name = "btnB"
+        Me.btnB.Size = New System.Drawing.Size(41, 50)
+        Me.btnB.TabIndex = 120
+        Me.btnB.Text = "B"
+        Me.btnB.UseVisualStyleBackColor = True
+        '
+        'btnV
+        '
+        Me.btnV.Location = New System.Drawing.Point(382, 525)
+        Me.btnV.Name = "btnV"
+        Me.btnV.Size = New System.Drawing.Size(41, 50)
+        Me.btnV.TabIndex = 119
+        Me.btnV.Text = "V"
+        Me.btnV.UseVisualStyleBackColor = True
+        '
+        'btnC
+        '
+        Me.btnC.Location = New System.Drawing.Point(335, 525)
+        Me.btnC.Name = "btnC"
+        Me.btnC.Size = New System.Drawing.Size(41, 50)
+        Me.btnC.TabIndex = 118
+        Me.btnC.Text = "C"
+        Me.btnC.UseVisualStyleBackColor = True
+        '
+        'btnX
+        '
+        Me.btnX.Location = New System.Drawing.Point(288, 525)
+        Me.btnX.Name = "btnX"
+        Me.btnX.Size = New System.Drawing.Size(41, 50)
+        Me.btnX.TabIndex = 117
+        Me.btnX.Text = "X"
+        Me.btnX.UseVisualStyleBackColor = True
+        '
+        'btnZ
+        '
+        Me.btnZ.Location = New System.Drawing.Point(241, 525)
+        Me.btnZ.Name = "btnZ"
+        Me.btnZ.Size = New System.Drawing.Size(41, 50)
+        Me.btnZ.TabIndex = 116
+        Me.btnZ.Text = "Z"
+        Me.btnZ.UseVisualStyleBackColor = True
+        '
+        'btnÑ
+        '
+        Me.btnÑ.Location = New System.Drawing.Point(595, 469)
+        Me.btnÑ.Name = "btnÑ"
+        Me.btnÑ.Size = New System.Drawing.Size(41, 50)
+        Me.btnÑ.TabIndex = 115
+        Me.btnÑ.Text = "Ñ"
+        Me.btnÑ.UseVisualStyleBackColor = True
+        '
+        'btnL
+        '
+        Me.btnL.Location = New System.Drawing.Point(548, 469)
+        Me.btnL.Name = "btnL"
+        Me.btnL.Size = New System.Drawing.Size(41, 50)
+        Me.btnL.TabIndex = 114
+        Me.btnL.Text = "l"
+        Me.btnL.UseVisualStyleBackColor = True
+        '
+        'btnK
+        '
+        Me.btnK.Location = New System.Drawing.Point(501, 469)
+        Me.btnK.Name = "btnK"
+        Me.btnK.Size = New System.Drawing.Size(41, 50)
+        Me.btnK.TabIndex = 113
+        Me.btnK.Text = "K"
+        Me.btnK.UseVisualStyleBackColor = True
+        '
+        'btnJ
+        '
+        Me.btnJ.Location = New System.Drawing.Point(454, 469)
+        Me.btnJ.Name = "btnJ"
+        Me.btnJ.Size = New System.Drawing.Size(41, 50)
+        Me.btnJ.TabIndex = 112
+        Me.btnJ.Text = "J"
+        Me.btnJ.UseVisualStyleBackColor = True
+        '
+        'btnH
+        '
+        Me.btnH.Location = New System.Drawing.Point(407, 469)
+        Me.btnH.Name = "btnH"
+        Me.btnH.Size = New System.Drawing.Size(41, 50)
+        Me.btnH.TabIndex = 111
+        Me.btnH.Text = "H"
+        Me.btnH.UseVisualStyleBackColor = True
+        '
+        'btnG
+        '
+        Me.btnG.Location = New System.Drawing.Point(360, 469)
+        Me.btnG.Name = "btnG"
+        Me.btnG.Size = New System.Drawing.Size(41, 50)
+        Me.btnG.TabIndex = 110
+        Me.btnG.Text = "G"
+        Me.btnG.UseVisualStyleBackColor = True
+        '
+        'btnF
+        '
+        Me.btnF.Location = New System.Drawing.Point(313, 469)
+        Me.btnF.Name = "btnF"
+        Me.btnF.Size = New System.Drawing.Size(41, 50)
+        Me.btnF.TabIndex = 109
+        Me.btnF.Text = "F"
+        Me.btnF.UseVisualStyleBackColor = True
+        '
+        'btnD
+        '
+        Me.btnD.Location = New System.Drawing.Point(266, 469)
+        Me.btnD.Name = "btnD"
+        Me.btnD.Size = New System.Drawing.Size(41, 50)
+        Me.btnD.TabIndex = 108
+        Me.btnD.Text = "D"
+        Me.btnD.UseVisualStyleBackColor = True
+        '
+        'btnS
+        '
+        Me.btnS.Location = New System.Drawing.Point(219, 469)
+        Me.btnS.Name = "btnS"
+        Me.btnS.Size = New System.Drawing.Size(41, 50)
+        Me.btnS.TabIndex = 107
+        Me.btnS.Text = "S"
+        Me.btnS.UseVisualStyleBackColor = True
+        '
+        'btnA
+        '
+        Me.btnA.Location = New System.Drawing.Point(172, 469)
+        Me.btnA.Name = "btnA"
+        Me.btnA.Size = New System.Drawing.Size(41, 50)
+        Me.btnA.TabIndex = 106
+        Me.btnA.Text = "A"
+        Me.btnA.UseVisualStyleBackColor = True
+        '
+        'btnP
+        '
+        Me.btnP.Location = New System.Drawing.Point(595, 404)
+        Me.btnP.Name = "btnP"
+        Me.btnP.Size = New System.Drawing.Size(41, 50)
+        Me.btnP.TabIndex = 105
+        Me.btnP.Text = "P"
+        Me.btnP.UseVisualStyleBackColor = True
+        '
+        'btnO
+        '
+        Me.btnO.Location = New System.Drawing.Point(548, 404)
+        Me.btnO.Name = "btnO"
+        Me.btnO.Size = New System.Drawing.Size(41, 50)
+        Me.btnO.TabIndex = 104
+        Me.btnO.Text = "O"
+        Me.btnO.UseVisualStyleBackColor = True
+        '
+        'btnI
+        '
+        Me.btnI.Location = New System.Drawing.Point(501, 404)
+        Me.btnI.Name = "btnI"
+        Me.btnI.Size = New System.Drawing.Size(41, 50)
+        Me.btnI.TabIndex = 103
+        Me.btnI.Text = "I"
+        Me.btnI.UseVisualStyleBackColor = True
+        '
+        'btnU
+        '
+        Me.btnU.Location = New System.Drawing.Point(454, 404)
+        Me.btnU.Name = "btnU"
+        Me.btnU.Size = New System.Drawing.Size(41, 50)
+        Me.btnU.TabIndex = 102
+        Me.btnU.Text = "U"
+        Me.btnU.UseVisualStyleBackColor = True
+        '
+        'btnY
+        '
+        Me.btnY.Location = New System.Drawing.Point(407, 404)
+        Me.btnY.Name = "btnY"
+        Me.btnY.Size = New System.Drawing.Size(41, 50)
+        Me.btnY.TabIndex = 101
+        Me.btnY.Text = "Y"
+        Me.btnY.UseVisualStyleBackColor = True
+        '
+        'btnT
+        '
+        Me.btnT.Location = New System.Drawing.Point(360, 404)
+        Me.btnT.Name = "btnT"
+        Me.btnT.Size = New System.Drawing.Size(41, 50)
+        Me.btnT.TabIndex = 100
+        Me.btnT.Text = "T"
+        Me.btnT.UseVisualStyleBackColor = True
+        '
+        'btnR
+        '
+        Me.btnR.Location = New System.Drawing.Point(313, 404)
+        Me.btnR.Name = "btnR"
+        Me.btnR.Size = New System.Drawing.Size(41, 50)
+        Me.btnR.TabIndex = 99
+        Me.btnR.Text = "R"
+        Me.btnR.UseVisualStyleBackColor = True
+        '
+        'btnE
+        '
+        Me.btnE.Location = New System.Drawing.Point(266, 404)
+        Me.btnE.Name = "btnE"
+        Me.btnE.Size = New System.Drawing.Size(41, 50)
+        Me.btnE.TabIndex = 98
+        Me.btnE.Text = "E"
+        Me.btnE.UseVisualStyleBackColor = True
+        '
+        'btnW
+        '
+        Me.btnW.Location = New System.Drawing.Point(219, 404)
+        Me.btnW.Name = "btnW"
+        Me.btnW.Size = New System.Drawing.Size(41, 50)
+        Me.btnW.TabIndex = 97
+        Me.btnW.Text = "W"
+        Me.btnW.UseVisualStyleBackColor = True
+        '
+        'btnQ
+        '
+        Me.btnQ.Location = New System.Drawing.Point(172, 404)
+        Me.btnQ.Name = "btnQ"
+        Me.btnQ.Size = New System.Drawing.Size(41, 50)
+        Me.btnQ.TabIndex = 96
+        Me.btnQ.Text = "Q"
+        Me.btnQ.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 110)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(803, 13)
+        Me.GroupBox1.TabIndex = 125
+        Me.GroupBox1.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(800, 574)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnELIMINAR)
+        Me.Controls.Add(Me.btnENVIAR)
+        Me.Controls.Add(Me.btnM)
+        Me.Controls.Add(Me.btnN)
+        Me.Controls.Add(Me.btnB)
+        Me.Controls.Add(Me.btnV)
+        Me.Controls.Add(Me.btnC)
+        Me.Controls.Add(Me.btnX)
+        Me.Controls.Add(Me.btnZ)
+        Me.Controls.Add(Me.btnÑ)
+        Me.Controls.Add(Me.btnL)
+        Me.Controls.Add(Me.btnK)
+        Me.Controls.Add(Me.btnJ)
+        Me.Controls.Add(Me.btnH)
+        Me.Controls.Add(Me.btnG)
+        Me.Controls.Add(Me.btnF)
+        Me.Controls.Add(Me.btnD)
+        Me.Controls.Add(Me.btnS)
+        Me.Controls.Add(Me.btnA)
+        Me.Controls.Add(Me.btnP)
+        Me.Controls.Add(Me.btnO)
+        Me.Controls.Add(Me.btnI)
+        Me.Controls.Add(Me.btnU)
+        Me.Controls.Add(Me.btnY)
+        Me.Controls.Add(Me.btnT)
+        Me.Controls.Add(Me.btnR)
+        Me.Controls.Add(Me.btnE)
+        Me.Controls.Add(Me.btnW)
+        Me.Controls.Add(Me.btnQ)
+        Me.Controls.Add(Me.btnconfig)
+        Me.Controls.Add(Me.btnopc)
+        Me.Controls.Add(Me.lbldeldia)
+        Me.Controls.Add(Me.lbllapalabra)
+        Me.Controls.Add(Me.btncal)
+        Me.Controls.Add(Me.btnbarras)
+        Me.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "Wordle"
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents btnbarras As Button
+    Friend WithEvents btncal As Button
+    Friend WithEvents lbllapalabra As Label
+    Friend WithEvents lbldeldia As Label
+    Friend WithEvents btnopc As Button
+    Friend WithEvents btnconfig As Button
+    Friend WithEvents btnELIMINAR As Button
+    Friend WithEvents btnENVIAR As Button
+    Friend WithEvents btnM As Button
+    Friend WithEvents btnN As Button
+    Friend WithEvents btnB As Button
+    Friend WithEvents btnV As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents btnX As Button
+    Friend WithEvents btnZ As Button
+    Friend WithEvents btnÑ As Button
+    Friend WithEvents btnL As Button
+    Friend WithEvents btnK As Button
+    Friend WithEvents btnJ As Button
+    Friend WithEvents btnH As Button
+    Friend WithEvents btnG As Button
+    Friend WithEvents btnF As Button
+    Friend WithEvents btnD As Button
+    Friend WithEvents btnS As Button
+    Friend WithEvents btnA As Button
+    Friend WithEvents btnP As Button
+    Friend WithEvents btnO As Button
+    Friend WithEvents btnI As Button
+    Friend WithEvents btnU As Button
+    Friend WithEvents btnY As Button
+    Friend WithEvents btnT As Button
+    Friend WithEvents btnR As Button
+    Friend WithEvents btnE As Button
+    Friend WithEvents btnW As Button
+    Friend WithEvents btnQ As Button
+    Friend WithEvents GroupBox1 As GroupBox
 End Class
--- Wordle/Form1.vb
+++ Wordle/Form1.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO
+Imports System.Linq.Expressions
 Imports WordleClases
 Public Class Form1
     Dim numeroFilas As Integer = 6
@@ -7,6 +8,10 @@ Public Class Form1
     Dim tamanoMargen As Integer = 5
     Dim indiceLabelActual As Integer = 0
     Dim indiceMaxCeldasRellenadasPorFila As Integer = numeroColumnas
+
+    Dim palabraFormando As String
+
+    Dim wordle As Diccionario
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim directorioSolucion As String = Path.GetDirectoryName(Path.GetDirectoryName(Application.StartupPath)) ''busca donde esta el .sln
         Dim rutaCompletaDirectorioPadreSln As String = Directory.GetParent(directorioSolucion).FullName ''te busca la ruta absoluta de cada ordenador hasta la carpeta que contiene el .sln
@@ -14,8 +19,10 @@ Public Class Form1
         Dim accesoFicherPalabras As String = Path.Combine(rutaPalabrasLeer, "Palabras.txt") ''de dicha ruta + carpeta accede al fichero Palabras.txt
 
 
-        Dim wordle As New Diccionario(accesoFicherPalabras) ''esto probablemente tenga que ser variable global...
+        wordle = New Diccionario(accesoFicherPalabras)
 
+        indiceLabelActual = Me.Controls.Count
+        indiceMaxCeldasRellenadasPorFila = numeroColumnas + indiceLabelActual
         For i As Integer = 0 To numeroFilas - 1
             For j As Integer = 0 To numeroColumnas - 1
                 Dim nuevoLabel As New Label()
@@ -31,8 +38,13 @@ Public Class Form1
             Next
         Next
 
+        For i As Integer = 0 To Me.Controls.Count - 1
+            If TypeOf Me.Controls(i) Is Label Then
+                Debug.WriteLine(i)
+            End If
+        Next
 
-
+        Me.Focus()
 
 
 
@@ -43,26 +55,90 @@ Public Class Form1
     Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
         If indiceLabelActual < numeroFilas * numeroColumnas Then
             Dim currentLabel As Label = CType(Me.Controls(indiceLabelActual), Label)
+            Debug.WriteLine($"Current Label: {indiceLabelActual}")
+            If e.KeyChar = ChrW(Keys.Back) AndAlso indiceLabelActual >= indiceMaxCeldasRellenadasPorFila - 5 Then
+                If palabraFormando.Length > 0 Then
+                    palabraFormando = palabraFormando.Substring(0, palabraFormando.Length - 1)
+                End If
 
+                If indiceLabelActual = indiceMaxCeldasRellenadasPorFila Then
+                    currentLabel = CType(Me.Controls(indiceLabelActual - 1), Label)
+                    currentLabel.Text = ""
+                    indiceLabelActual -= 2
+
+                Else
+
+                    If indiceLabelActual <> indiceMaxCeldasRellenadasPorFila - 5 Then
+                        indiceLabelActual -= 1
+
+                    End If
+                    currentLabel.Text = ""
+                End If
+
+
+
+                Debug.WriteLine(palabraFormando)
+
+            End If
 
             If e.KeyChar = ChrW(Keys.Enter) Then 'Si enter es pulsado mira si se ha completado la palabra o no
                 If indiceLabelActual <> indiceMaxCeldasRellenadasPorFila Then
                     MsgBox("Lenght != col")
                 Else
-                    'TODO mirar si esta en el diccionario
-                    indiceMaxCeldasRellenadasPorFila += 5
+                    palabraFormando = palabraFormando.Substring(0, 5)
+
+                    If wordle.palbraEsValida(palabraFormando) Then
+                        indiceMaxCeldasRellenadasPorFila += 5
+                        palabraFormando = ""
+                    Else
+                        MsgBox("La palabra no existe")
+                    End If
                 End If
                 Return
             End If
 
             If palabrasPermitidas.Contains(e.KeyChar.ToString.ToUpper) Then ''si no se ha pulsado eneter, se busca si la tecla esta en abecedario, si es asi haz ....
                 If indiceLabelActual <> indiceMaxCeldasRellenadasPorFila Then
-                    currentLabel.Text = e.KeyChar.ToString()
                     indiceLabelActual += 1
+                    currentLabel.Text = e.KeyChar.ToString()
+                    palabraFormando += currentLabel.Text
                 End If
             End If
 
             ''TODO al pressionar return eliminar palabras
+            Debug.WriteLine($"End Label: {indiceLabelActual}")
         End If
+    End Sub
+
+    Private Sub btn_Click(sender As Object, e As EventArgs) Handles btnQ.Click, btnW.Click, btnE.Click, btnR.Click, btnT.Click, btnY.Click, btnU.Click, btnI.Click, btnE.Click, btnO.Click, btnP.Click, btnA.Click, btnS.Click, btnD.Click, btnF.Click, btnG.Click, btnH.Click, btnJ.Click, btnK.Click, btnL.Click, btnÑ.Click, btnZ.Click, btnX.Click, btnC.Click, btnV.Click, btnB.Click, btnN.Click, btnM.Click
+
+        Dim button As Button = CType(sender, Button) ' Obtener el botón presionado
+        Dim currentLabel As Label = CType(Me.Controls(indiceLabelActual), Label) ' Obtener la etiqueta actual
+
+        If currentLabel > currentLabel + 5 Then
+            currentLabel.Text = button.Text ' Establecer el texto de la etiqueta a la letra del botón presionado
+            indiceLabelActual += 1 ' Incrementar el índice de etiqueta actual
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnENVIAR_Click(sender As Object, e As EventArgs) Handles btnENVIAR.Click 'Si boton enter es pulsado mira si se ha completado la palabra o no
+
+        If indiceLabelActual <> indiceMaxCeldasRellenadasPorFila Then
+            MsgBox("Lenght != col")
+        Else
+            palabraFormando = palabraFormando.Substring(0, 5)
+
+            If wordle.palbraEsValida(palabraFormando) Then
+                indiceMaxCeldasRellenadasPorFila += 5
+                palabraFormando = ""
+            Else
+                MsgBox("La palabra no existe")
+            End If
+        End If
+        Return
+
     End Sub
 End Class
